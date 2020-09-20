f7c7fdffb34a74fa7c7e5b5d98bd80be112e5138
jatury <jaturonmoonjan@gmail.com>
2020-09-20 14:27:37 +0700

    Commit HomePage ZipCode

--- App.js
+++ App.js
@@ -1,47 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Weather from './component/Weather'
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Weather zipCode="91110"/>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop:22,
-    // borderColor:'red',
-    // borderWidth:'red',
-    
-  },
-});
-
-
-
-
+// import { StatusBar } from 'expo-status-bar';
 // import React from 'react';
-// import { StyleSheet } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import ZipCodeScreen from './component/ZipCodeScreen';
-// import WeatherScreen from './component/WeatherScreen';
-
-// const Stack = createStackNavigator();
+// import { StyleSheet, Text, View } from 'react-native';
+// import Weather from './component/Weather'
 
 // export default function App() {
-//   return (        
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen name="Home" component={ZipCodeScreen} />
-//         <Stack.Screen name="Weather" component={WeatherScreen} />
-//       </Stack.Navigator> 
-//     </NavigationContainer>       
+//   return (
+//     <View style={styles.container}>
+//       <Weather zipCode="91110"/>
+//       <StatusBar style="auto" />
+//     </View>
 //   );
 // }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingTop:22,
+//     // borderColor:'red',
+//     // borderWidth:'red',
+    
+//   },
+// });
+
+
+
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ZipCodeScreen from './component/ZipCodeScreen';
+import WeatherScreen from './component/WeatherScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (        
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ZipCodeScreen} />
+        <Stack.Screen name="Weather" component={WeatherScreen} />
+      </Stack.Navigator> 
+    </NavigationContainer>       
+  );
+}
 
